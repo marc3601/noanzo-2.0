f2c99229ccb820ca3708a9312282f818bad3526b
Marcin Czaja <marcin.czaja2@gmail.com>
2024-03-19 19:38:58 +0100

    added generateSlugs url fixx

--- src/app/produkt/[slug]/page.tsx
+++ src/app/produkt/[slug]/page.tsx
@@ -21,7 +21,7 @@ export async function generateStaticParams() {
   const slugs = generateSlugs(params);
 
   return slugs.map((slug: Slug) => ({
-    slug: slug.id,
+    slug: slug.slug,
   }));
 }
 
