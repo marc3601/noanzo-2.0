486976ddbe165be184ef86fc3c31e391d8cfdf6d
Marcin <marcin.czaja2@gmail.com>
2026-01-25 13:33:40 +0100

    fix: handle escaped line breaks in TextWithBreaks component

--- src/app/components/TextWithBreaks.tsx
+++ src/app/components/TextWithBreaks.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 
 const TextWithBreaks = ({ text }: { text: string }) => {
-  let textArray = text.split(/\r\n/g);
-  if (textArray.length < 2) {
-    textArray = text.split(/\n/g);
-  }
+  // Handle both actual line breaks and escaped line breaks from JSON
+  const textArray = text
+    .split(/\\r\\n|\\n|\r\n|\n/g);
 
   return (
     <>
       {textArray.map((item, id) => {
-        if (item === "")
-          return (
-            <React.Fragment key={id}>
-              <br></br>
-            </React.Fragment>
-          );
-        return item;
+        const trimmedItem = item.trim();
+        
+        if (trimmedItem === "") {
+          return <br key={id} />;
+        }
+        
+        return (
+          <React.Fragment key={id}>
+            {trimmedItem}
+            <br />
+          </React.Fragment>
+        );
       })}
     </>
   );
